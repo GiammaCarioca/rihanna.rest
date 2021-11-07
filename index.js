b7724a5d1d10f3b815d7827b4d13512092b82890
Thiago Giammattey <giammattey@gmail.com>
2021-11-07 14:43:43 -0300

    allow GET method only

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const quotesRouter = require('./src/routes/quotes.routes')
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  "methods": "GET",
+}
+
+app.use(cors(corsOptions))
 app.use(express.static('public'))
 app.use('/quotes', quotesRouter)
 
