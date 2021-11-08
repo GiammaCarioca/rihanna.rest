42e3f30efeb6146fd8a4c90d11f09f401fc759bf
Thiago Giammattey <giammattey@gmail.com>
2021-11-08 09:29:16 -0300

    change some styles

--- public/index.js
+++ public/index.js
@@ -1,16 +1,24 @@
 const quote = document.querySelector("#quote")
 const btn = document.querySelector("#getQuote")
+const tweet = document.querySelector("#tweet")
 
 const base_url = "https://rihanna.rest"
 
 let quotes = []
+let tweet_url
+
+function rihtweet () {
+  window.open(tweet_url)
+}
 
 function renderFail () {
-  quote.innerHTML = "<p data-error><em>Something went wrong! Please try again.</em></p><img src='https://i.pinimg.com/originals/f4/2d/36/f42d36ef96e69be9adcfd4fb4c077508.gif' alt=''/><p>Never a failure, always a lesson.</p>"
+  quote.innerHTML = "<p data-error><em>Something went wrong! Please try again.</em></p><img src='https://i.pinimg.com/originals/f4/2d/36/f42d36ef96e69be9adcfd4fb4c077508.gif' alt=''/>"
 }
 
 async function getQuote () {
   try {
+    quote.innerHTML = "<em>Getting a fresh quote...</em>"
+
     const response = await fetch(`${base_url}/quotes`)
     
     if (!response.ok) throw response.status
@@ -30,6 +38,10 @@ async function getQuote () {
 
     quote.textContent = data[0]
     quotes.push(data[0])
+
+    tweet_url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      data[0] + " – Rihanna 🎈 rihanna.rest"
+      )}`
   } catch (error) {
     console.warn("Error:", error.status, error.statusText)
     renderFail()
@@ -38,4 +50,5 @@ async function getQuote () {
 
 getQuote();
 
-btn.addEventListener("click", getQuote);
+btn.addEventListener("click", getQuote)
+tweet.addEventListener("click", rihtweet)
